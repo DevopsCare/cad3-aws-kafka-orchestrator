230d20580881a7f7df8d5ac4189621d76804acdf
vkropotko <vjkropotko@gmail.com>
2019-10-31 17:58:00 +0300

    Fix outputs issue

--- output.tf
+++ output.tf
@@ -1,29 +1,27 @@
 output "kafka_instances" {
   value = merge(
-
-    { for idx in range(var.kafka_az1_count):
-       aws_route53_record.kafka_az1[idx].name => {
-         private_ip = module.kafka_nodes_az1.private_ip[idx]
-         az         = module.kafka_nodes_az1.availability_zone[idx]
-         id         = module.kafka_nodes_az1.id[idx]
+    { for idx in range(length(module.kafka_nodes_az1.id)):
+       element(aws_route53_record.kafka_az1, idx)["name"] => {
+         private_ip = element(module.kafka_nodes_az1.private_ip, idx)
+         az         = element(module.kafka_nodes_az1.availability_zone, idx)
+         id         = element(module.kafka_nodes_az1.id, idx)
        }
     },
-    { for idx in range(var.kafka_az2_count):
-       aws_route53_record.kafka_az2[idx].name => {
-         private_ip = module.kafka_nodes_az2.private_ip[idx]
-         az         = module.kafka_nodes_az2.availability_zone[idx]
-         id         = module.kafka_nodes_az2.id[idx]
+    { for idx in range(length(module.kafka_nodes_az2.id)):
+       element(aws_route53_record.kafka_az2, idx)["name"] => {
+         private_ip = element(module.kafka_nodes_az2.private_ip, idx)
+         az         = element(module.kafka_nodes_az2.availability_zone, idx)
+         id         = element(module.kafka_nodes_az2.id, idx)
        }
     },
-    { for idx in range(var.kafka_az3_count):
-       aws_route53_record.kafka_az3[idx].name => {
-         private_ip = module.kafka_nodes_az3.private_ip[idx]
-         az         = module.kafka_nodes_az3.availability_zone[idx]
-         id         = module.kafka_nodes_az3.id[idx]
+    { for idx in range(length(module.kafka_nodes_az3.id)):
+       element(aws_route53_record.kafka_az3, idx)["name"] => {
+         private_ip = element(module.kafka_nodes_az3.private_ip, idx)
+         az         = element(module.kafka_nodes_az3.availability_zone, idx)
+         id         = element(module.kafka_nodes_az3.id, idx)
        }
     }
   )
-
 }
 
 output "kafka_bootstrap_servers" {
@@ -32,32 +30,31 @@ output "kafka_bootstrap_servers" {
 
 output "zookeeper_instances" {
   value = merge(
-
-    { for idx in range(var.zookeeper_az1_count):
-       aws_route53_record.zookeeper_az1[idx].name => {
-         private_ip = module.zookeeper_nodes_az1.private_ip[idx]
-         az         = module.zookeeper_nodes_az1.availability_zone[idx]
-         id         = module.zookeeper_nodes_az1.id[idx]
+    { for idx in range(length(module.zookeeper_nodes_az1.id)):
+       element(aws_route53_record.zookeeper_az1, idx)["name"] => {
+         private_ip = element(module.zookeeper_nodes_az1.private_ip, idx)
+         az         = element(module.zookeeper_nodes_az1.availability_zone, idx)
+         id         = element(module.zookeeper_nodes_az1.id, idx)
        }
     },
-    { for idx in range(var.zookeeper_az2_count):
-       aws_route53_record.zookeeper_az2[idx].name => {
-         private_ip = module.zookeeper_nodes_az2.private_ip[idx]
-         az         = module.zookeeper_nodes_az2.availability_zone[idx]
-         id         = module.zookeeper_nodes_az2.id[idx]
+    { for idx in range(length(module.zookeeper_nodes_az2.id)):
+       element(aws_route53_record.zookeeper_az2, idx)["name"] => {
+         private_ip = element(module.zookeeper_nodes_az2.private_ip, idx)
+         az         = element(module.zookeeper_nodes_az2.availability_zone, idx)
+         id         = element(module.zookeeper_nodes_az2.id, idx)
        }
     },
-    { for idx in range(var.zookeeper_az3_count):
-       aws_route53_record.zookeeper_az3[idx].name => {
-         private_ip = module.zookeeper_nodes_az3.private_ip[idx]
-         az         = module.zookeeper_nodes_az3.availability_zone[idx]
-         id         = module.zookeeper_nodes_az3.id[idx]
+    { for idx in range(length(module.zookeeper_nodes_az3.id)):
+       element(aws_route53_record.zookeeper_az3, idx)["name"] => {
+         private_ip = element(module.zookeeper_nodes_az3.private_ip, idx)
+         az         = element(module.zookeeper_nodes_az3.availability_zone, idx)
+         id         = element(module.zookeeper_nodes_az3.id, idx)
        }
     }
   )
-
 }
 
 output "zookeeper_bootstrap_servers" {
   value = aws_route53_record.zookeeper_round_robin.name
 }
+
