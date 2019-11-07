9c9a12d71923e57bd2eef7e8bf90319dbeacae3b
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2019-11-07 14:22:34 +0200

    feature: fix ebs in zookeper, add instance prefixes

--- zookeeper.tf
+++ zookeeper.tf
@@ -16,73 +16,73 @@ data "template_cloudinit_config" "zookeeper" {
 }
 
 module "zookeeper_nodes_az1" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  version        = "~> 2.0"
+  instance_count = var.zookeeper_az1_count
 
-  name                        = "${module.zookeeper_label.id}-az1"
-  instance_count              = var.zookeeper_az1_count
-  ami                         = var.ami_id
-  instance_type               = var.zookeeper_instance_type
+  name          = "${module.zookeeper_label.id}-az1"
+  ami           = var.ami_id
+  instance_type = var.zookeeper_instance_type
+  tags          = module.zookeeper_label.tags
+  use_num_suffix  = true
+
   associate_public_ip_address = false
   subnet_id                   = var.az1_subnet_id
   vpc_security_group_ids      = concat([aws_security_group.zookeeper_cluster.id], var.zookeeper_additional_security_groups)
-  key_name                    = var.ec2_key_pair_name
 
+  key_name             = var.ec2_key_pair_name
   iam_instance_profile = var.zookeeper_instance_profile
+  user_data            = data.template_cloudinit_config.zookeeper.rendered
 
-  tags = module.zookeeper_label.tags
-
+  ebs_optimized     = true
   root_block_device = var.zookeeper_root_block_device
-
-  ebs_block_device = var.zookeeper_ebs_block_device
-
-  user_data = data.template_cloudinit_config.zookeeper.rendered
+  ebs_block_device  = var.zookeeper_ebs_block_device
 }
 
 module "zookeeper_nodes_az2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  version        = "~> 2.0"
+  instance_count = var.zookeeper_az2_count
 
-  name                        = "${module.zookeeper_label.id}-az2"
-  instance_count              = var.zookeeper_az2_count
-  ami                         = var.ami_id
-  instance_type               = var.zookeeper_instance_type
+  name          = "${module.zookeeper_label.id}-az2"
+  ami           = var.ami_id
+  instance_type = var.zookeeper_instance_type
+  tags          = module.zookeeper_label.tags
+  use_num_suffix  = true
+
   associate_public_ip_address = false
   subnet_id                   = var.az2_subnet_id
   vpc_security_group_ids      = concat([aws_security_group.zookeeper_cluster.id], var.zookeeper_additional_security_groups)
-  key_name                    = var.ec2_key_pair_name
 
+  key_name             = var.ec2_key_pair_name
   iam_instance_profile = var.zookeeper_instance_profile
+  user_data            = data.template_cloudinit_config.zookeeper.rendered
 
-  tags = module.zookeeper_label.tags
-
+  ebs_optimized     = true
   root_block_device = var.zookeeper_root_block_device
-
-  ebs_block_device = var.zookeeper_ebs_block_device
-
-  user_data = data.template_cloudinit_config.zookeeper.rendered
+  ebs_block_device  = var.zookeeper_ebs_block_device
 }
 
 module "zookeeper_nodes_az3" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  version        = "~> 2.0"
+  instance_count = var.zookeeper_az3_count
 
-  name                        = "${module.zookeeper_label.id}-az3"
-  instance_count              = var.zookeeper_az3_count
-  ami                         = var.ami_id
-  instance_type               = var.zookeeper_instance_type
+  name          = "${module.zookeeper_label.id}-az3"
+  ami           = var.ami_id
+  instance_type = var.zookeeper_instance_type
+  tags          = module.zookeeper_label.tags
+  use_num_suffix  = true
+
   associate_public_ip_address = false
   subnet_id                   = var.az3_subnet_id
   vpc_security_group_ids      = concat([aws_security_group.zookeeper_cluster.id], var.zookeeper_additional_security_groups)
-  key_name                    = var.ec2_key_pair_name
 
+  key_name             = var.ec2_key_pair_name
   iam_instance_profile = var.zookeeper_instance_profile
+  user_data            = data.template_cloudinit_config.zookeeper.rendered
 
-  tags = module.zookeeper_label.tags
-
+  ebs_optimized     = true
   root_block_device = var.zookeeper_root_block_device
-
-  ebs_block_device = var.zookeeper_ebs_block_device
-
-  user_data = data.template_cloudinit_config.zookeeper.rendered
+  ebs_block_device  = var.zookeeper_ebs_block_device
 }
