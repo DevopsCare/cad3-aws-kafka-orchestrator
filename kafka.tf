7cb3a72cbefaf22d3598a8875f60c9ef62dbeab0
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2019-11-07 13:59:36 +0200

    feature: fix ebs in kafka, add instance prefixes

--- kafka.tf
+++ kafka.tf
@@ -16,73 +16,73 @@ data "template_cloudinit_config" "kafka" {
 }
 
 module "kafka_nodes_az1" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  version        = "~> 2.0"
+  instance_count = var.kafka_az1_count
 
-  name                        = "${module.kafka_label.id}-az1"
-  instance_count              = var.kafka_az1_count
-  ami                         = var.ami_id
-  instance_type               = var.kafka_instance_type
+  name          = "${module.kafka_label.id}-az1"
+  ami           = var.ami_id
+  instance_type = var.kafka_instance_type
+  tags          = module.kafka_label.tags
+  use_num_suffix  = true
+
   associate_public_ip_address = false
   subnet_id                   = var.az1_subnet_id
   vpc_security_group_ids      = concat([aws_security_group.kafka_cluster.id], var.kafka_additional_security_groups)
-  key_name                    = var.ec2_key_pair_name
 
+  key_name             = var.ec2_key_pair_name
   iam_instance_profile = var.kafka_instance_profile
+  user_data            = data.template_cloudinit_config.kafka.rendered
 
-  tags = module.kafka_label.tags
-
+  ebs_optimized     = true
   root_block_device = var.kafka_root_block_device
-
-  ebs_block_device = var.kafka_ebs_block_device
-
-  user_data = data.template_cloudinit_config.kafka.rendered
+  ebs_block_device  = var.kafka_ebs_block_device
 }
 
 module "kafka_nodes_az2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  version        = "~> 2.0"
+  instance_count = var.kafka_az2_count
 
-  name                        = "${module.kafka_label.id}-az2"
-  instance_count              = var.kafka_az2_count
-  ami                         = var.ami_id
-  instance_type               = var.kafka_instance_type
+  name          = "${module.kafka_label.id}-az2"
+  ami           = var.ami_id
+  instance_type = var.kafka_instance_type
+  tags          = module.kafka_label.tags
+  use_num_suffix  = true
+
   associate_public_ip_address = false
   subnet_id                   = var.az2_subnet_id
   vpc_security_group_ids      = concat([aws_security_group.kafka_cluster.id], var.kafka_additional_security_groups)
-  key_name                    = var.ec2_key_pair_name
 
+  key_name             = var.ec2_key_pair_name
   iam_instance_profile = var.kafka_instance_profile
+  user_data            = data.template_cloudinit_config.kafka.rendered
 
-  tags = module.kafka_label.tags
-
+  ebs_optimized     = true
   root_block_device = var.kafka_root_block_device
-
-  ebs_block_device = var.kafka_ebs_block_device
-
-  user_data = data.template_cloudinit_config.kafka.rendered
+  ebs_block_device  = var.kafka_ebs_block_device
 }
 
 module "kafka_nodes_az3" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  version        = "~> 2.0"
+  instance_count = var.kafka_az3_count
 
-  name                        = "${module.kafka_label.id}-az3"
-  instance_count              = var.kafka_az3_count
-  ami                         = var.ami_id
-  instance_type               = var.kafka_instance_type
+  name          = "${module.kafka_label.id}-az3"
+  ami           = var.ami_id
+  instance_type = var.kafka_instance_type
+  tags          = module.kafka_label.tags
+  use_num_suffix  = true
+
   associate_public_ip_address = false
   subnet_id                   = var.az3_subnet_id
   vpc_security_group_ids      = concat([aws_security_group.kafka_cluster.id], var.kafka_additional_security_groups)
-  key_name                    = var.ec2_key_pair_name
 
+  key_name             = var.ec2_key_pair_name
   iam_instance_profile = var.kafka_instance_profile
+  user_data            = data.template_cloudinit_config.kafka.rendered
 
-  tags = module.kafka_label.tags
-
+  ebs_optimized     = true
   root_block_device = var.kafka_root_block_device
-
-  ebs_block_device = var.kafka_ebs_block_device
-
-  user_data = data.template_cloudinit_config.kafka.rendered
+  ebs_block_device  = var.kafka_ebs_block_device
 }
--- variables.tf
+++ variables.tf
@@ -52,7 +52,6 @@ variable "kafka_root_block_device" {
   default = [{
     volume_type           = "gp2"
     volume_size           = 10
-    delete_on_termination = true
   }]
 }
 
@@ -61,7 +60,6 @@ variable "kafka_ebs_block_device" {
     device_name           = "/dev/xvdb"
     volume_type           = "gp2"
     volume_size           = 30
-    delete_on_termination = true
   }]
 }
 
