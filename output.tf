74e9b6ad207cb1186825f0fdc6123d009ba11047
vkropotko <vjkropotko@gmail.com>
2019-11-25 14:58:28 +0200

    Make output parameter more specific: id ->  aws_instance_id

--- output.tf
+++ output.tf
@@ -2,23 +2,23 @@ output "kafka_instances" {
   value = merge(
     { for idx in range(length(module.kafka_nodes_az1.id)):
        element(aws_route53_record.kafka_az1, idx)["name"] => {
-         private_ip = element(module.kafka_nodes_az1.private_ip, idx)
-         az         = element(module.kafka_nodes_az1.availability_zone, idx)
-         id         = element(module.kafka_nodes_az1.id, idx)
+         private_ip      = element(module.kafka_nodes_az1.private_ip, idx)
+         az              = element(module.kafka_nodes_az1.availability_zone, idx)
+         aws_instance_id = element(module.kafka_nodes_az1.id, idx)
        }
     },
     { for idx in range(length(module.kafka_nodes_az2.id)):
        element(aws_route53_record.kafka_az2, idx)["name"] => {
-         private_ip = element(module.kafka_nodes_az2.private_ip, idx)
-         az         = element(module.kafka_nodes_az2.availability_zone, idx)
-         id         = element(module.kafka_nodes_az2.id, idx)
+         private_ip      = element(module.kafka_nodes_az2.private_ip, idx)
+         az              = element(module.kafka_nodes_az2.availability_zone, idx)
+         aws_instance_id = element(module.kafka_nodes_az2.id, idx)
        }
     },
     { for idx in range(length(module.kafka_nodes_az3.id)):
        element(aws_route53_record.kafka_az3, idx)["name"] => {
-         private_ip = element(module.kafka_nodes_az3.private_ip, idx)
-         az         = element(module.kafka_nodes_az3.availability_zone, idx)
-         id         = element(module.kafka_nodes_az3.id, idx)
+         private_ip      = element(module.kafka_nodes_az3.private_ip, idx)
+         az              = element(module.kafka_nodes_az3.availability_zone, idx)
+         aws_instance_id = element(module.kafka_nodes_az3.id, idx)
        }
     }
   )
@@ -32,23 +32,23 @@ output "zookeeper_instances" {
   value = merge(
     { for idx in range(length(module.zookeeper_nodes_az1.id)):
        element(aws_route53_record.zookeeper_az1, idx)["name"] => {
-         private_ip = element(module.zookeeper_nodes_az1.private_ip, idx)
-         az         = element(module.zookeeper_nodes_az1.availability_zone, idx)
-         id         = element(module.zookeeper_nodes_az1.id, idx)
+         private_ip      = element(module.zookeeper_nodes_az1.private_ip, idx)
+         az              = element(module.zookeeper_nodes_az1.availability_zone, idx)
+         aws_instance_id = element(module.zookeeper_nodes_az1.id, idx)
        }
     },
     { for idx in range(length(module.zookeeper_nodes_az2.id)):
        element(aws_route53_record.zookeeper_az2, idx)["name"] => {
-         private_ip = element(module.zookeeper_nodes_az2.private_ip, idx)
-         az         = element(module.zookeeper_nodes_az2.availability_zone, idx)
-         id         = element(module.zookeeper_nodes_az2.id, idx)
+         private_ip      = element(module.zookeeper_nodes_az2.private_ip, idx)
+         az              = element(module.zookeeper_nodes_az2.availability_zone, idx)
+         aws_instance_id = element(module.zookeeper_nodes_az2.id, idx)
        }
     },
     { for idx in range(length(module.zookeeper_nodes_az3.id)):
        element(aws_route53_record.zookeeper_az3, idx)["name"] => {
-         private_ip = element(module.zookeeper_nodes_az3.private_ip, idx)
-         az         = element(module.zookeeper_nodes_az3.availability_zone, idx)
-         id         = element(module.zookeeper_nodes_az3.id, idx)
+         private_ip      = element(module.zookeeper_nodes_az3.private_ip, idx)
+         az              = element(module.zookeeper_nodes_az3.availability_zone, idx)
+         aws_instance_id = element(module.zookeeper_nodes_az3.id, idx)
        }
     }
   )
