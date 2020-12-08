5e3be1dde17173260a59bf40adf39947bf1caec4
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-12-08 13:49:32 +0200

    feat: bump null-label to 0.22.0

--- labels.tf
+++ labels.tf
@@ -15,12 +15,12 @@
 */
 
 module "kafka_label" {
-  source  = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  source  = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.22.0"
   context = var.label.context
   name    = "kafka"
 }
 module "zookeeper_label" {
-  source  = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  source  = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.22.0"
   context = var.label.context
   name    = "zookeeper"
 }
